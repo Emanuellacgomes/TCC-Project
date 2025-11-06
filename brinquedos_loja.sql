3be483310332ddb580991d24d61b48efdaed5441
Emanuellacgomes <168464880+Emanuellacgomes@users.noreply.github.com>
2025-11-06 17:09:09 -0300

    Update brinquedos_loja.sql

--- brinquedos_loja.sql
+++ brinquedos_loja.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tempo de Geração: 05/11/2025 às 21h27min
+-- Tempo de Geração: 06/11/2025 às 21h08min
 -- Versão do Servidor: 5.5.20
 -- Versão do PHP: 5.3.9
 
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `brinquedos` (
   PRIMARY KEY (`codigo`),
   KEY `codcategoria` (`codcategoria`),
   KEY `codfabricante` (`codfabricante`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=41 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=42 ;
 
 --
 -- Extraindo dados da tabela `brinquedos`
@@ -47,11 +47,11 @@ CREATE TABLE IF NOT EXISTS `brinquedos` (
 INSERT INTO `brinquedos` (`codigo`, `nome`, `descricao`, `preco`, `estoque`, `imagem`, `codcategoria`, `codfabricante`) VALUES
 (4, 'Certo ou Errado?', 'O jogo "Certo ou Errado" da Estrela é um jogo de tabuleiro que desafia os jogadores a identificar se uma afirmação é verdadeira ou falsa. Ele pode ser jogado por 2 a 5 pessoas e é recomendado para maiores de 7 anos. O objetivo é avançar pelo tabuleiro respondendo corretamente às perguntas, com o jogador que chega ao final primeiro sendo o vencedor. O jogo é uma forma divertida e educativa de aprender curiosidades e fatos interessantes. ', '100.00', 3, '14431b5db842e1372a1b6ed7ae72570awebp', 7, 2),
 (5, 'Super Banco Imobiliário', 'O Jogo Banco Imobiliário permite que você invista muito bem o seu dinheiro em novas propriedades! Procura um jogo divertido para jogar com amigos e familiares? Esse modelo é perfeito, garante muito entretenimento e estimula o raciocínio lógico de todos!', '163.99', 33, '717786f0517dccdbb849dab6dd9e2758webp', 7, 2),
-(6, 'Pista de Carrinhos', 'Pista de carrinhos com looping e dois carrinhos de metal.', '89.90', 10, 'pista.png', 6, 5),
-(7, 'Boneca Clássica', 'Uma boneca com roupas de princesa.', '45.00', 25, 'boneca.png', 1, 4),
-(8, 'LEGO Casa na Árvore', 'Kit de construção LEGO com 500 peças para montar uma casa na árvore.', '250.00', 6, 'lego.png', 3, 5),
-(9, 'Jogo de Dominó', 'Jogo de dominó clássico com 28 peças de madeira.', '35.00', 50, 'domino.png', 7, 3),
-(10, 'Livro Sensorial', 'Livro de pano para bebês, com texturas e cores para estimular os sentidos.', '60.00', 30, 'livro_sensorial.png', 8, 2),
+(6, 'Pista de Carrinhos', 'Pista de carrinhos com looping e dois carrinhos de metal.', '89.90', 10, 'pista', 6, 5),
+(7, 'Boneca Clássica', 'Uma boneca com roupas de princesa.', '45.00', 25, 'boneca', 1, 4),
+(8, 'LEGO Casa na Árvore', 'Kit de construção LEGO com 500 peças para montar uma casa na árvore.', '250.00', 6, 'lego', 3, 5),
+(9, 'Jogo de Dominó', 'Jogo de dominó clássico com 28 peças de madeira.', '35.00', 50, 'domino', 7, 3),
+(10, 'Livro Sensorial', 'Livro de pano para bebês, com texturas e cores para estimular os sentidos.', '60.00', 30, 'livro_sensorial', 8, 2),
 (11, 'Pelúcia Ori', 'Uma pelúcia do amado personagem Ori da Franquia Ori and the Blind Forest perfeita para abraços!', '899.99', 0, 'da98027752910c871c7a398149ba806dwebp', 4, 1),
 (12, 'Colecionável Exclusivo - Angel - Lilo & Stich', 'Leve para casa toda a diversão e travessuras do Stitch com essa Miniatura Colecionável Oficial! Inspirada no adorável experimento 626 da Disney, essa peça traz detalhes incríveis que capturam sua personalidade única. - Design autêntico: Expressão fiel e acabamento impecável para um visual encantador. - Tamanho ideal: Compacta e perfeita para decorar sua estante, mesa ou coleção. - Presente perfeito: Para fãs de Lilo & Stitch, colecionadores da Disney ou qualquer um apaixonado por esse alienígena travesso! Adote o Stitch e leve um toque de aventura e fofura para o seu dia a dia!', '39.99', 27, '170b47ee710e8b491f727804803bd67fwebp', 5, 3),
 (13, 'Boneca Articulada e Acessórios - Wicked - Elphaba', 'Não há ninguém como Elphaba! Inspirada em Wicked da Universal Pictures, a boneca fashion Elphaba captura o coração da irmandade do filme. Vestida com sua moda removível fiel ao filme, mochila escolar e chapéu característico, Elphaba pode fazer poses dinâmicas com flexibilidade no tronco, cotovelos, pulsos e joelhos. Elphaba também apresenta seu visual icônico com detalhes fiéis ao filme, como seu cabelo longo e trançado e pele verde. Os fãs vão querer colecionar todas as bonecas da terra de Oz! Cada uma vendida separadamente, sujeita à disponibilidade. As bonecas não ficam em pé sozinhas. As cores e decorações podem variar.', '314.99', 0, 'af4e967fba984becfec5588eae176e28webp', 2, 4),
@@ -62,8 +62,8 @@ INSERT INTO `brinquedos` (`codigo`, `nome`, `descricao`, `preco`, `estoque`, `im
 (18, 'Boneca E Acessórios - Baby Alive - Swimmer - Roxo ', 'AVENTURAS AQUÁTICAS: Com a boneca Baby Alive Sunny Swimmer para meninos e meninas de 3 anos, as crianças vão se divertir com aventuras na água; ÓCULOS E NADADEIRA: A boneca inclui óculos e 2 nadadeiras para muita diversão na água; BRINCAR DENTRO E FORA D''ÁGUA: Linda boneca para brincadeiras divertidas na água para meninos e meninas a partir dos 3 anos: Boneca articulada com uma linda roupa de banho com estampas de criaturas marinhas para brincadeiras dentro e fora d''água; EXCELENTE PRESENTE PARA MENINOS E MENINAS: Estas bonecas aquáticas Baby Alive com acessórios são excelentes presentes de fim de ano ou de aniversário para crianças (Vendidos separadamente, sujeitos à disponibilidade.) A boneca Baby Alive Sunny Swimmer está pronta para muitas aventuras aquáticas. Esta boneca pra brincar na água é perfeita para meninos e meninas. Vem com óculos e 2 nadadeiras. A boneca articulada vem com uma linda roupa de banho com estampas de criaturas marinhas. É uma boneca divertida feita para brincar na água. Ideal para meninos e meninas a partir dos 3 anos. Estas bonecas aquáticas Baby Alive com acessórios são excelentes presentes de fim de ano ou de aniversário para crianças. (Vendidos separadamente, sujeitos à disponibilidade.)', '99.99', 134, '74066cbaa93e44ccbf74dc7cd64c30bawebp', 0, 5),
 (19, 'Boneca Minnie Patinadora com Sons - Elka - Disney', 'A Minnie adora patinar e passear com o Mickey! Aperte a estrelinha e ela fala frases. A Minnie tem patins e capacete para brincar e se divertir!', '149.99', 19, 'c63bae5ebdbe823bcd363d775f361e7dwebp', 0, 8),
 (20, 'LEGO Creator - Unicórnio Mágico 3 em 1 - 31140', 'Viaje para uma terra mítica distante para horas de diversão com estas criaturas lindas. Assista ao Unicórnio Mágico com seu chifre dourado cavalgar pela paisagem de arco-íris. Para ainda mais diversão encantadora, você pode reconstruir o modelo em um cavalo marinho nadando no mar ou transformá-lo em um pavão fofinho com uma cauda cheia de penas coloridas. A escolha é sua com este conjunto 3 em 1.', '99.99', 43, 'ad02ff8cde1be99ff346a501fbc66db5webp', 3, 6),
-(21, 'Ori and The Blind Forest: Estátua de PVC com varia', 'Arte inspirada: captura a essência da arte de capa icônica de Ori e a Floresta Cega, com Naru segurando Ori ternamente em um tronco caído\r\nVariação noturna: apresenta um esquema de cores escuras exclusivo para Naru e o tronco, perfeito para fãs que preferem a estética noturna do jogo\r\nFunções de iluminação LED: o brilho da Ori ganha vida com iluminação LED estática e animada na edição definitiva\r\nBase de resina detalhada: inclui detalhes expandidos como frutas, plantas e raízes de árvores espirituais, pintadas para combinar com o esquema de cores noturno\r\nPerfeito para decoração: adiciona um toque de capricho e calor à casa de qualquer fã, tornando-a uma peça decorativa ideal', '397.84', 6, '9d714d1625de26b13570b4cbcde92564.jpg', 3, 1),
-(22, 'Boneca Lego ', 'Uma Boneca feita de pecinhas lego para você montar em casa!', '120.99', 2, '836335a3be0234a9d199cad991bd9ef3.jpg', 0, 6),
+(21, 'Ori and The Blind Forest: Estátua de PVC', 'Arte inspirada: captura a essência da arte de capa icônica de Ori e a Floresta Cega, com Naru segurando Ori ternamente em um tronco caído\r\nVariação noturna: apresenta um esquema de cores escuras exclusivo para Naru e o tronco, perfeito para fãs que preferem a estética noturna do jogo\r\nFunções de iluminação LED: o brilho da Ori ganha vida com iluminação LED estática e animada na edição definitiva\r\nBase de resina detalhada: inclui detalhes expandidos como frutas, plantas e raízes de árvores espirituais, pintadas para combinar com o esquema de cores noturno\r\nPerfeito para decoração: adiciona um toque de capricho e calor à casa de qualquer fã, tornando-a uma peça decorativa ideal', '397.84', 6, '9d714d1625de26b13570b4cbcde92564', 3, 1),
+(22, 'Boneca Lego ', 'Uma Boneca feita de pecinhas lego para você montar em casa!', '120.99', 2, '836335a3be0234a9d199cad991bd9ef3', 0, 6),
 (23, 'Hora do Banho - Disney Baby - 14 Peças - Toyster', 'A linha BDA foi desenvolvida pensando no desenvolvimento e crescimento do seu bebê. São quatorze peças de espuma macia que, quando estão molhadas, grudam no azulejo. É uma forma divertida de conhecer os personagens da Disney na Hora do Banho.', '79.99', 34, 'a99af41c13f7e2018b7497c10cd69a96webp', 8, 9),
 (24, 'Jogo Educativo Pega Pompom Disney Pixar', 'As crianças amam pompons coloridos!! E não há nada mais divertido do que desenvolver habilidades brincado!! Atividade que desenvolve a coordenação motora fina, facilitando a prática da escrita, desenho e outras tarefas. Trabalha a concentração, reconhecimento de cores e criatividade.\r\nContém:56 pompons coloridos, 02 pinças plásticas e 04 placas de madeira dos personagens queridinhos dos filmes Disney Pixar.', '78.90', 12, '82f8477429d62757499988f1b76f0b8ewebp', 8, 9),
 (25, '  Piano e Xilofone - Disney Baby - Mickey e Amigos', 'O Piano Xilofone acompanha 2 baquetas para auxiliar na descoberta de novos sons • Acompanha alça na parte inferior para facilitar no transporte, fácil de carregar • As teclas do piano são macias e reproduzem sons vibrantes, atraindo a atenção do bebê • Auxilia no desenvolvimento de habilidades motoras, visuais, auditivas e imaginativa', '62.99', 0, '2c3e4088a8d96deece5c11c644d18885webp', 8, 9),
@@ -98,7 +98,7 @@ CREATE TABLE IF NOT EXISTS `carrinho_usuario` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_usuario` (`id_usuario`,`codigo_brinquedo`),
   KEY `codigo_brinquedo` (`codigo_brinquedo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 -- --------------------------------------------------------
 
@@ -192,7 +192,7 @@ CREATE TABLE IF NOT EXISTS `itens_pedido` (
   PRIMARY KEY (`id_item_pedido`),
   KEY `id_pedido` (`id_pedido`),
   KEY `codigo_brinquedo` (`codigo_brinquedo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
 
 --
 -- Extraindo dados da tabela `itens_pedido`
@@ -209,9 +209,13 @@ INSERT INTO `itens_pedido` (`id_item_pedido`, `id_pedido`, `codigo_brinquedo`, `
 (8, 91, 16, 1),
 (9, 92, 12, 1),
 (10, 92, 22, 1),
-(11, 93, 20, 1),
-(12, 93, 18, 2),
-(13, 93, 12, 2);
+(14, 94, 36, 1),
+(15, 94, 37, 1),
+(16, 95, 33, 1),
+(17, 95, 37, 1),
+(18, 96, 21, 1),
+(19, 97, 19, 1),
+(25, 102, 18, 1);
 
 -- --------------------------------------------------------
 
@@ -228,7 +232,7 @@ CREATE TABLE IF NOT EXISTS `pedidos` (
   `id_transacao_mp` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id_pedido`),
   KEY `id_usuario` (`id_usuario`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=94 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=103 ;
 
 --
 -- Extraindo dados da tabela `pedidos`
@@ -241,7 +245,11 @@ INSERT INTO `pedidos` (`id_pedido`, `id_usuario`, `data_pedido`, `valor_total`, 
 (90, 3, '2025-10-29 19:17:17', '26.25', 'pendente', '131137411495'),
 (91, 3, '2025-11-03 20:24:34', '1073.49', 'aprovado', '132350964452'),
 (92, 3, '2025-11-03 20:52:39', '120.74', 'pendente', '131755647237'),
-(93, 6, '2025-11-05 17:19:03', '379.95', 'pendente', NULL);
+(94, 3, '2025-11-06 18:53:07', '344.99', 'aprovado', '132154295639'),
+(95, 3, '2025-11-06 19:18:56', '269.99', 'cancelado', '132758952882'),
+(96, 6, '2025-11-06 19:32:15', '298.38', 'aprovado', '132158695673'),
+(97, 3, '2025-11-06 19:38:37', '112.49', 'aprovado', '132763217784'),
+(102, 6, '2025-11-06 20:26:01', '74.99', 'pendente', '132767561252');
 
 -- --------------------------------------------------------
 
